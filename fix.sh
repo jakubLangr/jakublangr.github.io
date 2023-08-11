bdad8c6c0a145344ac2cf9710905db8eb5675006
jakubLangr <james.langr@gmail.com>
2023-08-11 23:18:06 +0100

    fix vid

---
+++ fix.sh
@@ -0,0 +1,1 @@
+ find /Users/jlangr/code/jakublangr.github.io -type f -name "*.html" -exec sed -i '' 's|<a href="https://www\.youtube\.com/watch?v=a1Y73sPHKxw" class="button">LOL Wut|<a href="https://bit\.ly/gan-book" class="button">See my products|g' {} \;
